ade0c31b4474df3380f06f5e8debbced4feaab05
snowman-1030 <sinju1030@gmail.com>
2025-03-06 11:38:30 +0900

    Translate left

--- api/index.js
+++ api/index.js
@@ -1,20 +1,124 @@
 
 const express = require('express');
 const cors = require('cors');
+const { JSDOM } = require('jsdom');
 const TelegramBot = require('node-telegram-bot-api');
+const translate = require('@vitalets/google-translate-api');
 
 const PORT = process.env.PORT || 3000;
 const token = '8139148778:AAFNzYSpfqcA7dtekXu1VyOKOVkT6ccQSK4';
 const channelId = "@test_bot_channel_leo";
 
 const app = express();
-const bot = new TelegramBot(token, { polling: true,  request: { timeout: 20000 } });
+const bot = new TelegramBot(token, { polling: true, request: { timeout: 20000 } });
+
+var len = 0;
 
 app.use(cors());
 app.use(express.json());
 
+const getImg = (htmlString) => {
+
+    if(htmlString == "</a>") return "";
+
+    if(htmlString == "<br>") return "\n";
+
+    const dom = new JSDOM(htmlString);
+
+    let element = dom.window.document.querySelector('a');
+
+    if(element == null) {
+
+        element = dom.window.document.querySelector('img');
+
+        if(element == null){
+
+            console.log(htmlString);
+            
+            return null;
+        }
+
+        return element.alt;
+        
+    } else{
+        
+        link = element.href;
+        content = element.innerText;
+        
+        return `(${link})`;
+    }
+}
+
+const translateToFrench = async (text) => {
+    try {
+        const res = await translate(text, { from: 'pt', to: 'fr' });
+        console.log(`Original (Portuguese): ${text}`);
+        console.log(`Translated (French): ${res.text}`);
+    } catch (error) {
+        console.error('Error during translation:', error);
+    }
+}
+
 app.post('/send-message', (req, res) => {
+
+    // translateToFrench("Hi, How are you");
+
     const { message } = req.body; // Extract message from request body
+
+    let isHead = true;
+    let str = "";
+    let BotMessage = "";
+
+    if(len == message.length){
+        
+        res.status(200).json({ success: true, message: 'Already sent!' });
+
+        return;
+    }  
+
+    len = message.length;
+
+    for (let i = 0; i < len; i++) {
+        if (message[i] == ">" && isHead == true) {
+
+            isHead = false;
+
+            continue;
+        }
+
+        if (isHead == true) continue;
+
+        if (message[i] == ">") {
+
+            str = `${str}>`;
+            
+            let img = getImg(str);
+            
+            if(img == null) break;
+
+            BotMessage = `${BotMessage}${img}`;
+            str = "";
+
+            continue;
+        }
+
+        if (message[i] == "<") {
+
+            // if(str != " ") str = translateToFrench(str)
+            
+            BotMessage = `${BotMessage}${str}`;
+            str = "";
+
+        }
+
+        str = `${str}${message[i]}`;
+
+        if(str == "&nbsp;") str = " "; 
+
+    }
+
+    // console.log("=+++++++++++++++> ", BotMessage);
+    
 
     if (!message) {
         return res.status(400).json({ error: 'Message is required' });
